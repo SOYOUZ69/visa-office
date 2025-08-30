f21200144cac3ba3febf531cf76b2b83e63e4d62
Houssein Msallem <houssein.msallem@gmail.com>
2025-08-30 16:21:14 +0100

    added authguard to ai microservice

--- backend/src/aiservice/aiservice.controller.ts
+++ backend/src/aiservice/aiservice.controller.ts
@@ -1,26 +1,33 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { AiService } from './aiservice.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('api/v1/ai-service')
+@UseGuards(JwtAuthGuard)
 export class AiServiceController {
   constructor(private readonly visaService: AiService) {}
 
   @Get('countries')
+  @ApiBearerAuth()
   async getCountries() {
     return this.visaService.getCountries();
   }
 
   @Get('services')
+  @ApiBearerAuth()
   async getServices() {
     return this.visaService.getServices();
   }
 
   @Post('estimate')
+  @ApiBearerAuth()
   async getEstimate(@Body() estimateData: any) {
     return this.visaService.getEstimate(estimateData);
   }
 
   @Post('transactions')
+  @ApiBearerAuth()
   async createTransaction(@Body() transactionData: any) {
     return this.visaService.createTransaction(transactionData);
   }
